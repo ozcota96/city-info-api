49a4d38f54ff12b0433f8cabcff734b035bedcc9
Ozmar Cota <ozmar.cota@navix.io>
2024-04-03 20:15:26 -0600

    Adding cities and points of interest models and controllers

---
+++ Models/PointOfInterestDto.cs
@@ -0,0 +1,9 @@
+namespace CityInfoApi.Models
+{
+    public class PointOfInterestDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
